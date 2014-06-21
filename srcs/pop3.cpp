a2105637e1017f7e9b6fdbf518adf1a461b5cc7b
Geekuillaume <geek.uillaume@gmail.com>
2014-06-21 14:27:52 +0200

    Added pop3 mail parsing

--- srcs/pop3.cpp
+++ srcs/pop3.cpp
@@ -1,4 +1,5 @@
 #include "pop3.hpp"
+#include <regex>
 
 pop3::pop3() {};
 pop3::~pop3() {
@@ -42,12 +43,11 @@ void pop3::initConnexion(QString &user, QString &pass, QString &server, int port
 
 void pop3::getMails(std::function<void (std::vector<AMail *>)> callback)
 {
-    _mails.resize(15);
     for(unsigned i = 2; i < 15; ++i) {
         _socket.addNextCallback([this, i, callback] (QByteArray data) {
             pop3Mail *mail = new pop3Mail();
             mail->parseFromData(data);
-            _mails[i] = mail;
+            _mails.push_back(mail);
             if (i == 14)
             {
                 callback(_mails);
@@ -86,8 +86,13 @@ void pop3Mail::remove(std::function<void (std::string)> callback)
 
 void pop3Mail::parseFromData(QByteArray &data)
 {
-    _subject = "toto";
-    _sender = "tata";
-    _content = "contenu !";
+    int tmp;
+
+    tmp = data.indexOf("Subject: ");
+    _subject = data.mid(tmp + 9, data.indexOf("\n", tmp) - tmp - 9).data();
+    tmp = data.indexOf("To: ");
+    _sender = data.mid(tmp + 4, data.indexOf("\n", tmp) - tmp - 4).data();
+    tmp = data.indexOf("\r\n\r\n");
+    _content = data.mid(tmp + 4, data.size() - tmp - 9).data();
 }
 
